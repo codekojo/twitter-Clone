3fb47a412fa8a0cd122fecf11b0297f4b2099e34
Code Kojo <codekojo@gmail.com>
2021-04-20 22:24:22 +0530

    Fix Profile Image components height

--- App/components/ProfileImage.js
+++ App/components/ProfileImage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {Image} from 'react-native';
 import defaultStyle from '../config/defaultStyle';
 
-function ProfileImage() {
+function ProfileImage({uris}) {
   return (
     <Image
       source={{
-        uri: 'https://i1.sndcdn.com/artworks-000559377156-n3hr78-t500x500.jpg',
+        uri: uris,
       }}
       resizeMode="contain"
       style={defaultStyle.imageProfile}
