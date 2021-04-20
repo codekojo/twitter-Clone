3613a6de550a3c4dd94f1620c9c044646770d963
Code Kojo <codekojo@gmail.com>
2021-04-20 22:25:11 +0530

    Adjusted height

--- App/screens/Feed.js
+++ App/screens/Feed.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import defaultStyle from '../config/defaultStyle';
 import ProfileImage from '../components/ProfileImage';
 import FeedUser from '../components/Feed/FeedUser';
 import Tweets from '../components/Feed/Tweets';
 import TweetImage from '../components/Feed/TweetImage';
+import FeedInteraction from '../components/Feed/FeedInteraction';
 
-function Feed({TweetUrlImage}) {
+function Feed({
+  userName,
+  userHandle,
+  profileURL,
+  TweetUrlImage,
+  dateCreated,
+  tweet,
+  social,
+  tweetImage,
+  verified,
+}) {
   return (
     <View style={defaultStyle.feedContainer}>
       {/* Left Container */}
 
-      <View>
-        <ProfileImage />
+      <View style={defaultStyle.leftFeedContainer}>
+        <ProfileImage uris={profileURL} />
       </View>
 
       {/* Right Container */}
-      <View style={{flex: 1}}>
-        <FeedUser />
-        <Tweets />
-        {TweetUrlImage && <TweetImage />}
+      <View style={defaultStyle.RightFeedContainer}>
+        <FeedUser
+          username={userName}
+          userhandle={userHandle}
+          datecreated={dateCreated}
+          verifiedTrue={verified}
+        />
+        <Tweets tweets={tweet} />
+        {tweetImage && <TweetImage source={tweetImage} />}
+        <FeedInteraction socials={social} />
       </View>
     </View>
   );
