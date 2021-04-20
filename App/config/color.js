9dda435df5247745aae1d78d92efccedfb9c87a1
Code Kojo <codekojo@gmail.com>
2021-04-20 22:20:47 +0530

    Add new colour to the color

--- App/config/color.js
+++ App/config/color.js
@@ -1,3 +1,7 @@
 export default {
-  black: '#000',
+  blue: '#1DA1F2',
+  black: '#14171A',
+  darkGray: '#657786',
+  lightGray: '#AAB8C2',
+  extraLightGray: '#E1E8ED',
 };
--- App/config/defaultStyle.js
+++ App/config/defaultStyle.js
@@ -1,25 +1,61 @@
+import color from './color';
+
 export default {
   feedContainer: {
     flex: 1,
     flexDirection: 'row',
-    borderWidth: 1,
     padding: 10,
+  },
+  leftFeedContainer: {
+    marginRight: 10,
   },
   imageProfile: {
     height: 70,
     width: 70,
     borderRadius: 70 / 2,
   },
+  RightFeedContainer: {
+    flex: 1,
+  },
   feedUser: {
     width: '100%',
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: 'pink',
   },
-
   userInfo: {
     flexDirection: 'row',
     flex: 1,
     alignItems: 'center',
+  },
+  userName: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    paddingRight: 5,
+  },
+  userHandle: {
+    color: color.darkGray,
+    paddingLeft: 5,
+    textTransform: 'lowercase',
+    fontWeight: '500',
+  },
+  userTime: {
+    color: color.darkGray,
+    fontWeight: '500',
+  },
+  tweets: {
+    lineHeight: 20,
+    fontSize: 14,
+    color: color.dark,
+  },
+
+  feedInteraction: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 5,
+  },
+  itemSepator: {
+    height: 0.3,
+    backgroundColor: color.darkGray,
+    marginVertical: 5,
   },
 };
