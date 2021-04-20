fb427e0a911f5a5eaa1be420f2cef404ac866405
Code Kojo <codekojo@gmail.com>
2021-04-20 22:21:40 +0530

    Add an itemSeparator file for the Flatlist

---
+++ App/common/ItemSeparator.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native';
+import defaultStyle from '../config/defaultStyle';
+
+function ItemSeparator() {
+  return <View style={defaultStyle.itemSepator} />;
+}
+
+export default ItemSeparator;
